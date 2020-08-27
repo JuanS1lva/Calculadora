3b7a67d95910df9e6e74475b3b6580e40752fd40
JuanS1lva <juan.silva.vane@gmail.com>
2020-08-27 13:04:15 -0500

    calculadora beta 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let num2='';
 let operacion='';
 let resultado=0;
 let realizado=false;
+let punto=false;
 let teclado = document.getElementById("teclado")
 teclado.addEventListener("keydown",()=>{
     
@@ -12,55 +13,85 @@ caja.addEventListener("click",(e)=>{
         if(Number.isNaN(parseFloat(e.target.innerText))){
             const temp = e.target.innerText;
             if(e.target.innerText=='='){
-                console.log(num1)
-                console.log(num2)
                 switch (operacion) {
                     case '+':
                         resultado=parseFloat(num1)+parseFloat(num2);
-                        swal("Buen trabajo", "El resulado es: "+resultado, "success");
+                        swal("Buen trabajo", num1+" + "+num2+" = " +resultado, "success");
                         break;
                     case '-':
                         resultado=parseFloat(num1)-parseFloat(num2);
-                        swal("Buen trabajo", "El resulado es: "+resultado, "success");
+                        swal("Buen trabajo", num1+" - "+num2+" =" +resultado, "success");
                         break;
                     case 'x':
                         resultado=parseFloat(num1)*parseFloat(num2);
-                        swal("Buen trabajo", "El resulado es: "+resultado, "success");
+                        swal("Buen trabajo", num1+" * "+num2+" = " +resultado, "success");
+                        break;
+                    case '/':
+                        resultado=parseFloat(num1)/parseFloat(num2);
+                        if(Number.isNaN(resultado) || !Number.isFinite(resultado)){
+                            swal("Error!!", num1+" / "+num2+" = Math Error ("+resultado+")", "error");
+                            resultado="Math Error"
+                        }
+                        else{
+                            swal("Buen trabajo", num1+" / "+num2+" = " +resultado, "success");
+                        }
                         break;
                     default:
                         break;
                 }
                 operacion=''
-                teclado.value=resultado;
-                num1=String(resultado);
+                teclado.innerHTML=resultado;
+                if(resultado=="Math Error"){
+                    num1=''
+                }
+                else{
+                    num1=String(resultado);
+                }
                 num2='';
                 realizado=true;
             }
             else if(e.target.innerText=='AC'){
                 teclado.value = '0'
+                num1='';
+                num2='';
+            }
+            else if(e.target.innerText=='.'){
+                if(!punto){
+                    if(operacion==''){
+                        teclado.innerHTML = num1+e.target.innerText;
+                        num1=num1+e.target.innerText;
+                        punto=true;
+                    }
+                    else{
+                        teclado.innerHTML = num2+e.target.innerText;
+                        num2=num2+e.target.innerText;
+                        punto=true;
+                    }
+                }
             }
             else{
                 operacion= temp;
                 realizado=false;
+                punto=false;
             }
         }
         else{
             if(!realizado){
                 if(operacion==''){
                     const temp = e.target.innerText;
-                    teclado.value = num1+e.target.innerText;
+                    teclado.innerHTML = num1+e.target.innerText;
                     num1=num1+e.target.innerText;
                 }
                 else{
                     const temp = e.target.innerText;
-                    teclado.value = num2+e.target.innerText;
+                    teclado.innerHTML = num2+e.target.innerText;
                     num2=num2+e.target.innerText;
                 }
             }
             else{
                 if(operacion==''){
                     const temp = e.target.innerText;
-                    teclado.value = e.target.innerText;
+                    teclado.innerHTML = e.target.innerText;
                     num1=e.target.innerText;
                     realizado=false;
                 }
